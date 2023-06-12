125e462c2e6fbff5300cc1388040584ac0d08005
Feibrin <129807664+Feibrin@users.noreply.github.com>
2023-06-12 00:43:30 -0300

    Update Program.cs

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// Fabrício Nogueira Santods - 11/06/23
+using System;
 
 namespace Calculator
 {
@@ -8,13 +9,15 @@ namespace Calculator
         {
             char op;
             float num1, num2;
+// Solicita ao usuário que coloque o operador  
             Console.Write("ENTRE COM O OPERADOR: +, -, *, /: ");
             op = Console.ReadLine()[0];
+// Solicita ao usuário que coloque 2 valores separados por espaço            
             Console.Write("ENTRE COM DOIS VALORES: ");
             string[] values = Console.ReadLine().Split(' ');
             num1 = float.Parse(values[0]);
             num2 = float.Parse(values[1]);
-
+// Realiza a operação com base no operador digitado e exibe o resultado
             switch (op)
             {
                 case '+':
