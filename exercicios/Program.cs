b98b252ec7c8c55c5675f2c5a4bcc0df8ce6610e
Feibrin <129807664+Feibrin@users.noreply.github.com>
2023-06-12 00:45:47 -0300

    Update Program.cs

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -8,10 +8,11 @@ class Program
 
         do
         {
+// Solicita ao usuário que digite um número entre 1 e 10            
             Console.Write("DIGITE UM NUMERO ENTRE 1 A 10: ");
             num = int.Parse(Console.ReadLine());
         } while (num < 1 || num > 10);
-
+// Imprime a mensagem informando o número digitado pelo usuário
         Console.WriteLine("VOCÊ DIGITOU O NÚMERO " + num + ".");
     }
 }
