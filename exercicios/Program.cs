b7bef56ebcf0376bf1fbd6a83832bba2b98c8538
Feibrin <129807664+Feibrin@users.noreply.github.com>
2023-06-12 00:56:29 -0300

    Update Program.cs

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -2,24 +2,25 @@
 
 class Program
 {
+// Função para realizar a soma de dois números inteiros    
     static int Soma(int a, int b)
     {
         int resultado = a + b;
         return resultado;
     }
-
+// Função para realizar a subtração de dois números inteiros
     static int Subtracao(int a, int b)
     {
         int resultado = a - b;
         return resultado;
     }
-
+// Função para realizar a multiplicação de dois números inteiros
     static int Multiplicacao(int a, int b)
     {
         int resultado = a * b;
         return resultado;
     }
-
+// Função para realizar a divisão de dois números inteiros
     static int Divisao(int a, int b)
     {
         int resultado = a / b;
@@ -28,6 +29,7 @@ class Program
 
     static void Main(string[] args)
     {
+// Chamada das funções e exibição dos resultados        
         Console.WriteLine(Soma(30, 5));
         Console.WriteLine(Subtracao(30, 5));
         Console.WriteLine(Multiplicacao(30, 5));
