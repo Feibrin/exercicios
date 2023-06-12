edaae17829348f14c96686fdf48a5c8ade2eace0
Feibrin <129807664+Feibrin@users.noreply.github.com>
2023-06-12 01:00:14 -0300

    Update Program.cs

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -8,10 +8,12 @@ class Program
 
         Console.Write("Digite um número inteiro positivo: ");
         num = int.Parse(Console.ReadLine());
-
+// Repetição enquanto o valor de i for menor ou igual ao número informado
         while (i <= num)
         {
+// Imprime o valor de i seguido de um espaço            
             Console.Write(i + " ");
+// Incrementa o valor de i em 1 para a próxima iteração            
             i++;
         }
     }
