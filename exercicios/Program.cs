3d2eaf5cf846f65627311e1c6168dcd456cb4d68
Fabrício Santos <fabrinnsantos@gmail.com>
2023-06-12 00:03:53 -0300

    update

--- exercicios/Program.cs
+++ exercicios/Program.cs
@@ -2,35 +2,16 @@
 
 class Program
 {
-    static int Soma(int a, int b)
-    {
-        int resultado = a + b;
-        return resultado;
-    }
-
-    static int Subtracao(int a, int b)
-    {
-        int resultado = a - b;
-        return resultado;
-    }
-
-    static int Multiplicacao(int a, int b)
-    {
-        int resultado = a * b;
-        return resultado;
-    }
-
-    static int Divisao(int a, int b)
-    {
-        int resultado = a / b;
-        return resultado;
-    }
-
     static void Main(string[] args)
     {
-        Console.WriteLine(Soma(30, 5));
-        Console.WriteLine(Subtracao(30, 5));
-        Console.WriteLine(Multiplicacao(30, 5));
-        Console.WriteLine(Divisao(30, 5));
+        int num;
+
+        do
+        {
+            Console.Write("DIGITE UM NUMERO ENTRE 1 A 10: ");
+            num = int.Parse(Console.ReadLine());
+        } while (num < 1 || num > 10);
+
+        Console.WriteLine("VOCÊ DIGITOU O NÚMERO " + num + ".");
     }
 }
